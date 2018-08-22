de922f8171a6f675c2cee2cf4a05f7ac5e28e36c
hexeth <7627137+hexeth@users.noreply.github.com>
2018-08-22 12:47:42 -0700

    Added more classes to ModalWanted array

--- CSS/caliBlur.js
+++ CSS/caliBlur.js
@@ -311,7 +311,7 @@ $( '#logout' ).parent().addClass( 'dropdown' ).appendTo( '.profileDropli' );
 
 // Remove the modals except from some areas where they are needed
 bodyClass = $( 'body' ).attr( 'class' ).split(' ');
-modalWanted = ['admin', 'editbook'];
+modalWanted = ['admin', 'editbook', 'config', 'uiconfig'];
 
 if ( $.inArray( bodyClass[0], modalWanted) === 1 ) {
 } else {
