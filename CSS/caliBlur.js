2e8ec0764f7ffe188ab18c6a8a5c20852478b2fc
Matthew Sorensen <msorensen@local.gthiringsolutions.ca>
2018-08-24 09:26:19 -0700

    removing add to shelves code

--- CSS/caliBlur.js
+++ CSS/caliBlur.js
@@ -397,14 +397,13 @@ $( '#btn-upload' ).attr({
   'data-viewport': '#main-nav' })
   .addClass('upload-btn-tooltip');
 
-if ( !$( 'body.search' ).length === 1 ) {
 $( '#add-to-shelf' ).attr({
   'data-toggle': 'tooltip',
   'title': 'Add to Shelf',
   'data-placement': 'bottom',
   'data-viewport': '.btn-group' })
   .addClass('addtoshelf-btn-tooltip');
-}
+
 $( '#have_read_cb' ).attr({
   'data-toggle': 'tooltip',
   'title': 'Mark As Read',
