e0665f4fac623ddb6360967ab1ffa5ace676ccb7
Matthew Sorensen <msorensen@local.gthiringsolutions.ca>
2018-08-24 14:33:38 -0700

    try and fix adding to shelf

--- CSS/caliBlur.js
+++ CSS/caliBlur.js
@@ -283,7 +283,7 @@ $(document).on('click','.dropdown-toggle',function() {
 
 // Fade out content on page unload
 // delegate all clicks on "a" tag (links)
-$(document).on("click", "a", function () {
+$(document).on("click", "a:not(.btn-toolbar a)", function () {
 
     // get the href attribute
     var newUrl = $(this).attr("href");
