e752f1605624395b0c610424d6165baec22206f3
hexeth <7627137+hexeth@users.noreply.github.com>
2018-08-29 11:28:28 -0700

    .

--- CSS/caliBlur.js
+++ CSS/caliBlur.js
@@ -378,7 +378,7 @@ $(document).on('click','.dropdown-toggle',function() {
 });
 
 // Fade out content on page unload
- delegate all clicks on "a" tag (links)
+// delegate all clicks on "a" tag (links)
 $(document).on("click", "a:not(.btn-toolbar a, a[href*='shelf/remove'], .identifiers a, .bookinfo , .btn-group > a )", function () {
 
     // get the href attribute
