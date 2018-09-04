3cbf5453231fb50a8efa2db24b4eaa9ed3d7f876
hexeth <7627137+hexeth@users.noreply.github.com>
2018-09-04 09:37:07 -0700

    move .navbar-brand for mobile

--- CSS/caliBlur.js
+++ CSS/caliBlur.js
@@ -638,9 +638,11 @@ function mobileSupport() {
 if ( $( window ).width() < 769 ) {
   //Move menu to collapse
     $( '.row-fluid > .col-sm-2' ).appendTo( '.navbar-collapse.collapse');
+    $( '.navbar-brand' ).insertBefore(  '.nav.navbar-nav' );
    } else {
   //Move menu out of collapse
     $( '.col-sm-2:first' ).insertBefore( '.col-sm-10');
+    $('.navbar-brand').appendTo( '.navbar-header' );
    }
 }
 
