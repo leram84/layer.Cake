f6e47e6c6c0aebc54fbd5f5f1d70192b38073076
hexeth <7627137+hexeth@users.noreply.github.com>
2018-10-02 08:11:55 -0700

    boop

--- CSS/caliBlur.js
+++ CSS/caliBlur.js
@@ -483,7 +483,7 @@ $(document).on('click','.dropdown-toggle',function() {
 
 // Fade out content on page unload
 // delegate all clicks on "a" tag (links)
-$(document).on("click", "a:not(.btn-toolbar a, a[href*='shelf/remove'], .identifiers a, .bookinfo , .btn-group > a, #add-to-shelves a )", function () {
+$(document).on("click", "a:not(.btn-toolbar a, a[href*='shelf/remove'], .identifiers a, .bookinfo , .btn-group > a, #add-to-shelves a, #book-list a )", function () {
 
     // get the href attribute
     var newUrl = $(this).attr("href");
